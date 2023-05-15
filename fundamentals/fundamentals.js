245738710d67b4a160f32f54f68e1f009907aabe
Gustavout22 <gustavougartetorres@gmail.com>
2023-05-15 18:51:40 -0500

    add: solution makeNegative

--- fundamentals/fundamentals.js
+++ fundamentals/fundamentals.js
@@ -1,9 +1,16 @@
 // FUNDAMENTALS EXERCISES
 // functiom
-function IsDivisible(n, x, y) {
+function isDivisible(n, x, y) {
   return (n % x == 0 && n % y == 0)
 };
 
 // form arrow function
 const isDivisible = (n, x, y) =>
   n % x === 0 && n % y === 0;
+
+// Return negative
+function makeNegative(num) {
+  return (num > 0) ? -num : num
+};
+//arrow function
+const makeNegative = (num) => (num > 0) ? -num : num
