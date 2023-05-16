496fef4b727042e0fb4d8b7a545452d2ab1ef8ca
Gustavout22 <gustavougartetorres@gmail.com>
2023-05-15 19:10:02 -0500

    add: solution perfect square

--- fundamentals/fundamentals.js
+++ fundamentals/fundamentals.js
@@ -3,14 +3,26 @@
 function isDivisible(n, x, y) {
   return (n % x == 0 && n % y == 0)
 };
-
 // form arrow function
 const isDivisible = (n, x, y) =>
   n % x === 0 && n % y === 0;
 
-// Return negative
+
+// RETURN NEGATIVE 
 function makeNegative(num) {
   return (num > 0) ? -num : num
 };
 //arrow function
 const makeNegative = (num) => (num > 0) ? -num : num
+
+
+// YOUR'RE SQUARE
+var isSquare = function(n){
+    return Number.isInteger(Math.sqrt(n)) ?  true : false  
+   }
+   
+function isSquare(n) {
+     return Number.isInteger(Math.sqrt(n))
+}
+   
+const isSquare = (n) => Number.isInteger(Math.sqrt(n)) ?  true : false
