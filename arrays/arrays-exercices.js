90c407e45e6d3eec9f2c2b58b360870f4285a69d
Gustavout22 <gustavougartetorres@gmail.com>
2023-05-17 23:34:25 -0500

    add: solution sum two small numbers of array

--- arrays/arrays-exercices.js
+++ arrays/arrays-exercices.js
@@ -6,7 +6,13 @@ function abbrevName(name){
   return result.join('.')
   }
 
-//   You only need one - Beginner
+//   You only need one - Beginner || No Loops 2 - You only need one
 function check(a, x) {
     return a.includes(x)
   }
+//  Sum of two lowest positive integers
+function sumTwoSmallestNumbers(numbers) {
+    numbers.sort(function(a, b){return a - b})
+    return numbers[0] + numbers[1]
+  }
+  
