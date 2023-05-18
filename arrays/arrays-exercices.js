c5d461de980bc8ce14df69b5f8e45375c0e94f25
Gustavout22 <gustavougartetorres@gmail.com>
2023-05-17 22:03:02 -0500

    add: solution check value in array

--- arrays/arrays-exercices.js
+++ arrays/arrays-exercices.js
@@ -1,7 +1,12 @@
-// Abrebiate Name
+// Abrebbiate a two word Name
 function abbrevName(name){
     result = name.split(" ").map(function(ele){
           return ele[0].toUpperCase()       
     })
   return result.join('.')
+  }
+
+//   You only need one - Beginner
+function check(a, x) {
+    return a.includes(x)
   }
