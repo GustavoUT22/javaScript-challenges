dac90aa423d63c80ba1f9d667fc2e666172c555b
Gustavout22 <gustavougartetorres@gmail.com>
2023-05-20 02:14:22 -0500

    add: solution hash to array

--- objects-as-instances/objects-instances-exercices.js
+++ objects-as-instances/objects-instances-exercices.js
@@ -4,4 +4,12 @@ const Ball = function(ballType = "regular") {
   };
   
   const ball1 = new Ball()
-  ball1.ballType
+  const ball2 = new Ball("super")
+  ball1.ballType // it would be regular by default
+  ball2.ballType  // it would be super because if they pass argument
+
+//Convert Hash to an Array
+function convertHashToArray(hash){
+    const resultado = Object.entries(hash).sort((a, b) => a[0].localeCompare(b[0]));
+    return resultado
+  }
