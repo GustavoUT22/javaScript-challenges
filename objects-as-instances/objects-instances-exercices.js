67feff822ae61a5c539de997ce0b1b83a0f3f296
Gustavout22 <gustavougartetorres@gmail.com>
2023-05-20 13:49:01 -0500

    add: solution fix my method

--- objects-as-instances/objects-instances-exercices.js
+++ objects-as-instances/objects-instances-exercices.js
@@ -27,3 +27,14 @@ function likes(names) {
       return `${names[0]}, ${names[1]} and ${(names.length - 2)} others like this`
     }
   }
+  //Fix my method
+  function myFunction() {
+    var myObject = {
+      objProperty: "string",
+      objMethod: function objMethod() {
+        return myObject.objProperty
+      }
+    };
+    
+    return myObject;
+  }
