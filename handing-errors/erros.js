3712cdea20e7e5b40e94cefc45a08478eada3114
Gustavout22 <gustavougartetorres@gmail.com>
2023-05-29 16:07:42 -0500

    add: solution errors completed

---
+++ handing-errors/erros.js
@@ -0,0 +1,13 @@
+function validateMessage(msg) {
+  if (msg === null) {
+    throw new ReferenceError('Message is null!');
+  }
+  if (typeof msg !== 'string') {
+    throw new TypeError(`Message should be of type string but was of type ${typeof msg}!`);
+  }
+  if (msg.length === 0 || msg.length > 255) {
+    throw new RangeError(`Message contains ${msg.length} characters!`);
+  }
+  const htmlTagsRegex = /<[^>]+>/g;
+  return !htmlTagsRegex.test(msg);
+}
